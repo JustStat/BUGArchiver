54ba00024b53cb5c848aa258ed3a435a67e79c5b
Natalia <Myopintemp@gmail.com>
2015-07-13 17:00:06 +1000

    main
    with workin input

--- main.cpp
+++ main.cpp
@@ -7,8 +7,39 @@
 //
 
 #include <iostream>
+#include <cstdio>
+#include "input.h"
+#include "Archivation.h"
 
-int main(int argc, const char * argv[]) {
-    
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    data* input_data = new data;
+    using_any_option(argc, argv, input_data);
+    if(input_data->type!="c" && input_data->type!="d")
+    {
+        cout <<"\nError. Wrong type of operation, use key -h";
+        return 0;
+    }
+    int error;
+    vector<byte> buffer;
+    if(input_data->have_input_file)
+        error = reading(input_data, buffer);
+    else
+    {
+        cout <<"\nError. There is no file name.";
+        return 0;
+    }
+    if(error)
+    {
+        cout <<"\nError. No such file in directory";
+        return 0;
+    }
+    cout << buffer.size();
+    for(int i=0; i<buffer.size(); i++)
+    {
+        cout << buffer[i] << " ";
+    }
     return 0;
 }
